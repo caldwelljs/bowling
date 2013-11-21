4806b9684e3327c25d2c5371e5341bfb61c36a95
John Caldwell <caldwelljs@gmail.com>
2013-11-21 11:07:49 -0600

    add passsing zero score test

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,10 +1,17 @@
 class BowlingGame
   attr_accessor :rolls
+
   def initialize
     @rolls = []
   end
 
-  # def rolls
-  #   rolls << @rolls
-  # end
+  def roll(pins)
+
+  end
+
+  def score
+    0
+  end
+
+
 end
